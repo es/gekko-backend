6b5c657f47e900163a774a3ffcbcac0f94b7fb42
Emil Stolarsky <emil.stolarsky@gmail.com>
2014-11-15 21:16:14 -0500

    Rearrange app.use() calls

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(app.router);
 app.use(cors(corsOptions));
+app.use(app.router);
 
 // development only
 if ('development' == app.get('env')) {
